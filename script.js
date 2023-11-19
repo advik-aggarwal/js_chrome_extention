6ddf86d9555b28ec1c7055dfcb06b9af285ece43
Advik_Aggarwal <92903318+advik-aggarwal@users.noreply.github.com>
2023-11-19 22:40:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let myLeads = ['hello','whasas','suppppp','rolando']
 const inputEL = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
+let listItems = ""
 
 
 inputBtn.addEventListener("click" , function() {
@@ -9,10 +10,12 @@ inputBtn.addEventListener("click" , function() {
  console.log(myLeads)
 })
 
-for (let i = 0; i < myLeads.length ; i++) {
-    // ulEl.innerHTML += "<li>" +  myLeads[i] + ' ' + "</li>"
 
-    const li = document.createElement("li")
-    li.textContent = myLeads[i]
-    ulEl.append(li)
+for (let i = 0; i < myLeads.length; i++) {
+    listItems += "<li>" + myLeads[i] + "</li>"
+
+    // const li = document.createElement("li")
+    // li.textContent = myLeads[i]
+    // ulEl.append(li)
 }
+ulEl.innerHTML = listItems
